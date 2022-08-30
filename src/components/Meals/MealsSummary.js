aa0ca8538b68dab475026de7eb5c2824eaefcbe7
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-08-29 22:11:17 -0400

    start layout for app

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,5 +1,7 @@
+import classes from './MealsSummary.module.css';
+
 const MealsSummary = () => {
-   return <section>
+   return <section className={classes.summary}>
     <h2>Delicious Food, Delivered To You</h2>
       <p>
         Choose your favorite meal from our broad selection of available meals
