b5f16f65c3df70919567617266d4081bf5e19a34
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-08-30 22:03:28 -0400

    added Card component for styling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,3 +1,4 @@
+import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 const DUMMY_MEALS = [
   {
@@ -30,7 +31,9 @@ const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
   return (
     <section className={classes.meals}>
+      <Card>
       <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
