93dd41e5120359d3d85bebc357c3e2143385c973
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-09-10 00:49:10 -0400

    added form add to app

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -10,7 +10,9 @@ const MealItem = (props) => {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div><MealItemForm/></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 };
