d8a18e58834d0f13600ff35560d2d968fbb99017
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-09-20 21:13:55 -0400

    use context for cart management

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import { Fragment, useState } from 'react';
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import Cart from './components/Cart/Cart'
-import CartProvider from './store/CartProvider';
+import { Fragment, useState } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 function App() {
-
   const [cartIsShown, setCartIsShown] = useState(false);
 
-  const showCartHandler = ()=>{
-    setCartIsShown(true)
-  }
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
 
-  const hideCartHandler = ()=>{
+  const hideCartHandler = () => {
     setCartIsShown(false);
-  }
+  };
 
   return (
     <CartProvider>
-    {cartIsShown && <Cart onClose = {hideCartHandler}/>}
-    <Header onShowCart = {showCartHandler}></Header>
-    <main>
-    <Meals></Meals>
-    </main>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler}></Header>
+      <main>
+        <Meals></Meals>
+      </main>
     </CartProvider>
   );
 }
