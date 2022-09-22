5a6db1ce06b15a57495bc266db35504254f0baf1
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-09-21 23:50:00 -0400

    cart modal

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,22 @@
+import { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
-import classes from './HeaderCartButton.module.css'
-const HeaderCartButton = props =>{
-   return (<button className = {classes.button} onClick = {props.onClick}>
-    <span className={classes.icon}>
-    <CartIcon/>
-    </span>
-    <span>Your Cart</span>
-    <span className={classes.badge}>3</span>
-   </button>
-   )
-}
+import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
+
+const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.items.reduce((accu, current) => {
+    return accu + current.amount;
+  }, 0);
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
 
 export default HeaderCartButton;
