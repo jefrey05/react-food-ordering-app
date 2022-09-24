46ab87c40a96131fa7ab441a724457b65566647e
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-09-24 00:06:32 -0400

    addding items to cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,24 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
+const defaultCartState = {
+  items:[],
+  totalAmount: 0
+}
+const cartReducer = (state,action)=>{
+   
+  return defaultCartState;
+}
 const CartProvider = (props) => {
+
+  const [cartState, dispatchCartAction] = useReducer(cartReducer,defaultCartState);
   const addItemToCartHandler = (item) => {};
 
   const removeItemCartHandler = (id) => {};
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemCartHandler,
   };
